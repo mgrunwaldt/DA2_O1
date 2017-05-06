2e91740b3e297f46b7fd773dcdc4d177b00be690
Matias Grunwaldt <matigru@gmail.com>
2017-05-06 11:30:20 -0300

    Product Data Access Test

---
+++ Obligatorio 1/RepositoryTests/ProductAccessTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RepositoryTests
+{
+    [TestClass]
+    public class ProductAccessTests
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            Product p = new Product();
+            ProductsRepository pr = new ProductsRepository();
+            pr.Add(p);
+            Assert.AreNotEqual(Guid.Empty, p.Id);
+        }
+
+        [TestMethod]
+        public void GetTest()
+        {
+            Product p = new Product();
+            ProductsRepository pr = new ProductsRepository();
+            pr.Add(p);
+            Assert.Equals(pr.Get(p.Id), p);
+        }
+
+        [TestMethod]
+        public void GetAllTest()
+        {
+            Product p1 = new Product();
+            Product p2 = new Product();
+            Product p3 = new Product();
+            ProductsRepository pr = new ProductsRepository();
+            pr.Add(p1);
+            pr.Add(p2);
+            pr.Add(p3);
+            var products = pr.GetAll();
+            Assert.Equals(products.Count(),4);
+            Assert.Equals(products[0], p1);
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            Product p1 = new Product();
+            Product p2 = new Product();
+            ProductsRepository pr = new ProductsRepository();
+            pr.Add(p1);
+            pr.Add(p2);
+            pr.Delete(p1.Id);
+            var products = pr.GetAll();
+            Assert.Equals(products.Count(), 1);
+            Assert.Equals(products[0], p2);
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            Product p1 = new Product();
+            p1.Name = "Producto 1";
+            p1.Code = "123456";
+            p1.Description = "Descripcion p1";
+            p1.Manufacturer = "Fabricante p1";
+            p1.Price = 100;
+            ProductsRepository pr = new ProductsRepository();
+            pr.Add(p1);
+            p1.Name = "Producto 1 Actualizado";
+            pr.Update(p1.Id, p1);
+            var product = pr.Get(p1.Id);
+            Assert.Equals(product.Name, "Producto 1 Actualizado");
+        }
+    }
+}
