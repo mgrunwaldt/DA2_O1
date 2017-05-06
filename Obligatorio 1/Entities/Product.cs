0069bd4d5a328982a28d01c22c92033b87424785
Matias Grunwaldt <matigru@gmail.com>
2017-05-06 14:05:32 -0300

    Data Access Tests - Compila pero no ada

---
+++ Obligatorio 1/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Product
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public Guid Id { get; set; }
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ Obligatorio 1/Repository/ProductsRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace Repository
+{
+    public class ProductsRepository
+    {
+        public void Add(Product p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Guid id, Product p1)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
