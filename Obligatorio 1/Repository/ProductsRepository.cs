32f788324f597df1e9766a7567b9d4f16b707a09
MATIASGRUNWC07C\Mati <matigru@gmail.com>
2017-05-07 22:16:18 -0300

    Git Ignore

---
+++ Obligatorio 1/Repository/ProductsRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+using DataAccess;
+
+namespace Repository
+{
+    public class ProductsRepository
+    {
+        public void Add(Product p)
+        {
+            using (var context = new MyContext()) {
+                p.Id = Guid.NewGuid();
+                context.Products.Add(p);
+                context.SaveChanges();
+            }
+        }
+        //HOLA
+        public Product Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Guid id, Product p1)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
