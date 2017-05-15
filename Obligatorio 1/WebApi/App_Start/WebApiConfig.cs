eec4d769f0dc26a8c2d6fdf696b923dca7e92368
MATIASGRUNWC07C\Mati <matigru@gmail.com>
2017-05-15 10:35:51 -0300

    Comienzo Web Api, Creación de proyecto, de proyecto de pruebas y configuración de inyección de dependencias

---
+++ Obligatorio 1/WebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Repository;
+using Entities;
+using DataAccess;
+
+namespace WebApi
+{
+    public static class WebApiConfig
+    {
+
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            var container = new UnityContainer();
+            MyContext context = new MyContext();
+            GenericRepository<User> userRepo = new GenericRepository<User>(context);
+            GenericRepository<Address> addressRepo = new GenericRepository<Address>(context);
+            container.RegisterType<IGenericRepository<User>, GenericRepository<User>>(new HierarchicalLifetimeManager());
+            container.RegisterInstance<GenericRepository<User>>(userRepo);
+            container.RegisterInstance<GenericRepository<Address>>(addressRepo);
+            config.DependencyResolver = new UnityResolver(container);
+
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Obligatorio 1/WebApi/Controllers/UsersController.cs
@@ -0,0 +1,105 @@
+﻿using Entities;
+using Repository;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Exceptions;
+
+namespace WebApi.Controllers
+{
+    /*
+     private IProductRepository _repository;
+
+    public ProductsController(IProductRepository repository)  
+    {
+        _repository = repository;
+    }
+         */
+     
+    public class UsersController : ApiController
+    {
+        private IGenericRepository<User> _userRepository;
+        private IGenericRepository<Address> _addressRepository;
+
+        private IUserService _userService;
+        public UsersController(IGenericRepository<User> userRepo,IGenericRepository<Address> addressRepo) {
+            _userRepository = userRepo;
+            _addressRepository = addressRepo;
+            _userService = new UserService(_userRepository, _addressRepository);
+        }
+
+        [Route("api/Users/Register", Name = "Register")]
+        [HttpPost]
+        public IHttpActionResult Register(User user)
+        {
+            try
+            {
+                if (user != null)
+                {
+                    _userService.Register(user, user.Address);
+                    return Ok("Usuario Registrado con éxito");
+                }
+                return BadRequest("Debes enviar todos los datos");
+
+            }
+            catch (MissingUserDataException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (ExistingUserException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (WrongPasswordException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (WrongNumberFormatException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (WrongEmailFormatException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+        /*
+          [EnableCors(origins: "*", headers: "*", methods: "*")]
+        [Route("api/Users/Register", Name="Register")]
+        [HttpPost]
+        [ResponseType(typeof(User))]
+        public IHttpActionResult Register(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            int id = userService.CreateUser(user);
+            return Ok(user);
+        }
+         */
+        /*
+         public IEnumerable<Product> Get()
+    {
+        return _repository.GetAll();
+    }
+
+    public IHttpActionResult Get(int id)
+    {
+        var product = _repository.GetByID(id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+        return Ok(product);
+    }
+         */
+    }
+}
